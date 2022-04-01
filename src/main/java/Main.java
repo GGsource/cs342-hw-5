d437afcf58b3ed69ba0214613552c198dd843b12
ggonza55 <ggonza55@uic.edu>
2022-03-31 21:22:12 -0500

    Finished HW5

---
+++ src/main/java/Main.java
@@ -0,0 +1,36 @@
+
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			// Read file fxml and draw interface.
+			Parent root = FXMLLoader.load(getClass()
+					.getResource("/FXML/Myfxml.fxml"));
+	
+			primaryStage.setTitle("G Gonzalez Homework 5"); //Set window title and icon
+			primaryStage.getIcons().add(new Image("file:src/main/resources/images/p5.png"));
+
+			Scene s1 = new Scene(root);
+			s1.getStylesheets().add("/styles/style1.css");
+			primaryStage.setScene(s1);
+			primaryStage.show();
+		} 
+		catch(Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/main/java/MyController.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+public class MyController implements Initializable {
+    @FXML
+    private TextField inputField;
+    @FXML
+    private TextField outputField;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		inputField.setPromptText("enter text here then press button 1");
+        outputField.setText("final string goes here");
+        outputField.setDisable(true); //Output should always be disabled
+	}	
+	public void button1Method(ActionEvent e) throws IOException {
+        outputField.setText(inputField.getText()+": from the center text field!"); //Get user input and set it on outputField
+        System.out.println("User had: " + inputField.getText());
+	}
+    public void button2Method(ActionEvent e) throws IOException {
+        inputField.clear();
+        outputField.setText("final string goes here");
+        System.out.println("Cleared fields!");
+    }
+}
