23f9cec08fbf1ff319746ed3d660d16533992084
ggonza55 <ggonza55@uic.edu>
2022-03-31 21:32:33 -0500

    Added comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,9 @@
-
-	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-
 
 public class Main extends Application {
 	@Override
--- src/main/java/MyController.java
+++ src/main/java/MyController.java
@@ -14,17 +14,17 @@ public class MyController implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		inputField.setPromptText("enter text here then press button 1");
-        outputField.setText("final string goes here");
+		inputField.setPromptText("enter text here then press button 1"); //Tell user what to do
+        outputField.setText("final string goes here"); //Specifically not a prompt
         outputField.setDisable(true); //Output should always be disabled
 	}	
 	public void button1Method(ActionEvent e) throws IOException {
         outputField.setText(inputField.getText()+": from the center text field!"); //Get user input and set it on outputField
-        System.out.println("User had: " + inputField.getText());
+        //System.out.println("User had: " + inputField.getText());
 	}
     public void button2Method(ActionEvent e) throws IOException {
-        inputField.clear();
-        outputField.setText("final string goes here");
-        System.out.println("Cleared fields!");
+        inputField.clear(); //Clear input so prompt shows again
+        outputField.setText("final string goes here"); //Reset message in output
+        //System.out.println("Cleared fields!");
     }
 }
